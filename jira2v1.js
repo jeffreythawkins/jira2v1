08aedd99dca30feb9c571975660a6682a7a2de73
Jeff Hawkins <jeffreythawkins@gmail.com>
2018-05-14 10:48:05 -0600

    New Feature: Only Add Unique

--- jira2v1.js
+++ jira2v1.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 const axios = require('axios');
 
-var escape = require("escape-html");
+var escape = require('escape-html');
 
 const {JIRAUSER,JIRAPASS,V1USER,V1PASS} = process.env; // Your account usernames and passwords.
 
@@ -13,15 +13,18 @@ const STORY_ID = process.env.STORY_ID;            // 876543 (Not the S-12345 loo
 const JIRA_JQL = process.env.JIRA_JQL;            // 'project = abc AND status in (open) and issuetype in (defect)'
 
 
-const V1_TASK_URI = V1_BASE_URI + '/rest-1.v1/Data/Task';
-const V1_LINK_URI = V1_BASE_URI + '/rest-1.v1/Data/Link';
+const V1_REST_TASK_URI = V1_BASE_URI + '/rest-1.v1/Data/Task';
+const V1_REST_LINK_URI = V1_BASE_URI + '/rest-1.v1/Data/Link';
+const V1_QUERY_STORY_URI = V1_BASE_URI + '/query.v1/Data/Story';
 const JIRA_URI = JIRA_BASE_URI + '/rest/api/2/search';
 
 const JIRA_MAX_RESULTS = 50;
+const V1_MAX_RESULTS = 50;
 
 axios.defaults.auth = {username: V1USER, password: V1PASS};
 
-getJiraDefects(createJiraSearchRequestBody()).then ( defects => {
+
+getJiraDefects().then ( defects => {
     createV1TasksFromDefects(defects).then (tasks => {
         console.log(tasks);
     });
@@ -30,6 +33,17 @@ getJiraDefects(createJiraSearchRequestBody()).then ( defects => {
 //
 // FUNCTIONS
 //
+
+async function getJiraDefects() {
+  let requestBody = createJiraSearchRequestBody()
+  try {
+    response = await axios.post(JIRA_URI,requestBody,{auth:{username:JIRAUSER,password:JIRAPASS}})
+    return response.data.issues.map(transformDefect);
+  }
+  catch (err) {
+    console.log('Error in getJiraDefects: ' + err);
+  }
+}
 
 function createJiraSearchRequestBody () {
   return {
@@ -46,16 +60,6 @@ function createJiraSearchRequestBody () {
       'reporter',
       'description'
     ]
-  }
-}
-
-async function getJiraDefects(requestBody) {
-  try {
-    response = await axios.post(JIRA_URI,requestBody,{auth:{username:JIRAUSER,password:JIRAPASS}})
-    return response.data.issues.map(transformDefect);
-  }
-  catch (err) {
-    console.log('Error in getJiraDefects: ' + err);
   }
 }
 
@@ -82,6 +86,70 @@ function createHTMLLink(href,text) {
   return `<![CDATA[<a href=${href}>${text}</a>]]>`;
 }
 
+async function createV1TasksFromDefects(defects) {
+  try { 
+    defects = await filterOutPreviouslyProcessedDefects(defects);
+    if (defects.length == 0) 
+      return {};
+
+    const allPromises = defects.map(createNewV1Task);
+    let tasks = await Promise.all(allPromises);
+    return tasks;
+  } catch (err) {
+    console.log('Error in createV1TasksFromDefects: ' + err);
+  }
+}
+
+async function filterOutPreviouslyProcessedDefects(defects) {
+  try {
+    let searchRequestBody=createV1FindTasksInStoryRequestBody();
+    const response = await axios.post(V1_QUERY_STORY_URI,searchRequestBody);
+    let tasks = response.data[0];
+
+    defects = applyFilter(defects,tasks);
+
+    return defects;
+  } catch (err) {
+    console.log('Error in filterOutPreviouslyProcessedDefects: ' + err);
+  }
+}
+
+function createV1FindTasksInStoryRequestBody() {
+  return {
+      "from": "Task",
+      "select": [
+          "Number",
+          "Name",
+      ],
+      "where": {
+          "Parent.ID":"Story:"+STORY_ID
+      },
+      "page": {
+          "start": 0,
+          "size": V1_MAX_RESULTS    
+      }
+  };
+}
+
+function applyFilter(defects,tasks) {  
+  return defects.filter(defect =>  !tasks.some(task => task.Name.includes(defect.key))) 
+}
+
+async function createNewV1Task(defect) {
+  try {
+    let taskRequestBody = createNewV1TaskRequestBody(defect);
+    const taskResponse = await axios.post(V1_REST_TASK_URI,taskRequestBody);
+    let taskId = taskResponse.data.id;
+    taskId=taskId.split(':')[1];
+   
+    const linkId = await createNewV1Link(defect,taskId);
+    
+    return `${taskId}`;
+  } catch (err) {
+    console.log('Error in createNewV1Task ' + err);
+  }
+}
+
 function createNewV1TaskRequestBody(defect) {
   return [
       `<Asset href="/rest-1.v1/New/Task">`,
@@ -94,28 +162,15 @@ function createNewV1TaskRequestBody(defect) {
   ].join('');
   }
 
-async function createV1TasksFromDefects(defects) {
-  try {  
-    const allPromises = defects.map(createV1TaskFromDefect);
-    let tasks = await Promise.all(allPromises);
-    return tasks;
-  } catch (err) {
-    console.log('Error in createV1TasksFromDefects: ' + err);
-  }
-}
-
-async function createV1TaskFromDefect(defect) {
+async function createNewV1Link(defect,taskId) {
   try {
-    let taskRequestBody = createNewV1TaskRequestBody(defect);
-    const taskResponse = await axios.post(V1_TASK_URI,taskRequestBody);
-    let taskId = taskResponse.data.id;
-    taskId=taskId.split(':')[1];
-   
     let linkRequestBody = createNewV1LinkRequestBody('JIRA',defect.url,taskId)
-    let linkResponse = await axios.post(V1_LINK_URI, linkRequestBody);
-    return `${taskId}`;
+    let linkResponse = await axios.post(V1_REST_LINK_URI, linkRequestBody);
+    let linkId = linkResponse.data.id;
+    linkId=linkId.split(':')[1];
+    return linkId;
   } catch (err) {
-    console.log('Error in createV1TaskFromDefect' + err);
+    console.log('Error in createNewV1Link ' + err);
   }
 }
 
