188f08595de19e439b9da8018b434cd9afffdb99
Clif Bergmann <cbergmann@familysearch.org>
2018-05-16 08:49:53 -0600

    Enhance functionality
    Incorporate semistandard and make autofixes.
    Minor re-ordering.
    Add pre-commit hook to run standards via husky.
    Add quick logging for debugging.
    Add JIRA_UNSET_PRIORITY_VALUE, V1_TASK_DETAIL, V1_TASK_HOURS env options.
    Add TODOs.

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 # jira2v1
 
-This node script copies matching Atlassian JIRA defects and creates a task for each on a VersionOne story. It ignores JIRAs that may have already been added from a previous run.
+This node script searches an Atlassian JIRA instance for defects and creates a task for each in a VersionOne story. It ignores JIRAs that may have already been added from a previous run.
 
 ## Install
 
-Clone this repository. In the script directory, run: 
+Clone this repository. In the root of the project, run:
 
 `npm install`
 
-The script uses the package 'dotenv' to store your username and password for both JIRA and VersionOne. Create an .env file in the script directory. It should have these entries: 
+The script uses the package 'dotenv' to store your username and password for both JIRA and VersionOne. Create an .env file in the script directory with the following entries:
 
 .env
-````
+
+```
 V1USER={username}
 V1PASS={password}
 JIRAUSER={username}
@@ -20,19 +21,34 @@ JIRAPASS={password}
 V1_BASE_URI = 'http://www.yourv1.com'
 JIRA_BASE_URI = 'http://www.yourjira.com'
 STORY_ID = 876543 (Not the S-12345 looking thing)
-JIRA_JQL = 'project = abc AND status in (open) and issuetype in (defect)'
-````
+JIRA_JQL = 'project = abc AND status in (open) AND type = Defect AND resolution = Unresolved'
+```
+
+Optional Entries:
+
+```
+V1_TASK_HOURS={number of hours you would like each task to be. Default: 4}
+V1_TASK_DETAIL=short
+```
 
 ## Configure
 
-* Change the JIRA_JQL in the .env file. This is the JIRA query statement. The resulting defects that will be copied to V1 as Tasks with Links. 
-* Change the STORY_ID in the .env file. This is the target story where all tasks will be created. This is not the S-109579 looking thingy. It's the id found in the URL for the story.
+* Change the JIRA_JQL in the .env file. This is the JIRA query statement. The resulting defects that will be copied to V1 as Tasks with links.
+* Change the STORY_ID in the .env file. This is the target story where all tasks will be created. This is not the S-109579 looking thingy. It's the id found in the URL for the story if you click on it.
 
 
-## Run 
+## Run
 
+```
+npm start
+```
+
+or
+
+```
 node jira2v1
+```
 
-## Observe 
+## Observe
 
-Take a long draw on the beverage of your choice. Your flatmate might appreciate a comment about the lovely weather.
+Take a long draw on the beverage of your choice. Your flatmate might appreciate a comment about the lovely weather. At your leisure, inspect the taskboard, and note the recent additions.
--- jira2v1.js
+++ jira2v1.js
@@ -1,17 +1,19 @@
+const axios = require('axios');
 const dotenv = require('dotenv');
+const escape = require('escape-html');
+
 dotenv.config();
 
-const axios = require('axios');
+const {JIRAUSER, JIRAPASS, V1USER, V1PASS} = process.env; // Your account usernames and passwords.
+axios.defaults.auth = {username: V1USER, password: V1PASS};
 
-var escape = require('escape-html');
-
-const {JIRAUSER,JIRAPASS,V1USER,V1PASS} = process.env; // Your account usernames and passwords.
-
-const V1_BASE_URI = process.env.V1_BASE_URI;      // http://www.yourv1.com
-const JIRA_BASE_URI = process.env.JIRA_BASE_URI;  // http://www.yourjira.com
-const STORY_ID = process.env.STORY_ID;            // 876543 (Not the S-12345 looking thing)
-const JIRA_JQL = process.env.JIRA_JQL;            // 'project = abc AND status in (open) and issuetype in (defect)'
-
+const JIRA_BASE_URI = process.env.JIRA_BASE_URI; // http://www.yourjira.com
+const JIRA_JQL = process.env.JIRA_JQL; // 'project = abc AND status in (open) and issuetype in (defect)'
+const JIRA_UNSET_PRIORITY_VALUE = process.env.JIRA_UNSET_PRIORITY_VALUE || 6; // Your organization's "Not Set" equivalent. Default: 6
+const V1_BASE_URI = process.env.V1_BASE_URI; // http://www.yourv1.com
+const V1_STORY_ID = process.env.V1_STORY_ID; // 876543 (Not the S-12345 looking thing)
+// const V1_TASK_HOURS = process.env.V1_TASK_HOURS || 4; // Default: 4
+const V1_TASK_DETAIL = process.env.V1_TASK_DETAIL || 'long'; // Default: 'long'; Options 'short'|'long'
 
 const V1_REST_TASK_URI = V1_BASE_URI + '/rest-1.v1/Data/Task';
 const V1_REST_LINK_URI = V1_BASE_URI + '/rest-1.v1/Data/Link';
@@ -21,35 +23,42 @@ const JIRA_URI = JIRA_BASE_URI + '/rest/api/2/search';
 const JIRA_MAX_RESULTS = 50;
 const V1_MAX_RESULTS = 50;
 
-axios.defaults.auth = {username: V1USER, password: V1PASS};
+getJiraDefects().then(defects => {
+  // // DEBUG: Just spit out what was fetched from JIRA
+  // defects.map((defect) => {
+  //   console.log(defect.title);
+  // });
+  // return;
 
-
-getJiraDefects().then ( defects => {
-    createV1TasksFromDefects(defects).then (tasks => {
-        console.log(tasks);
+  createV1TasksFromDefects(defects).then(tasks => {
+    tasks.map((task) => {
+      if (task) {
+        console.log(task);
+      }
     });
+  });
 });
 
 //
 // FUNCTIONS
 //
 
-async function getJiraDefects() {
-  let requestBody = createJiraSearchRequestBody()
+async function getJiraDefects () {
+  let requestBody = createJiraSearchRequestBody();
   try {
-    response = await axios.post(JIRA_URI,requestBody,{auth:{username:JIRAUSER,password:JIRAPASS}})
+    let response = await axios.post(JIRA_URI, requestBody, {auth: {username: JIRAUSER, password: JIRAPASS}});
     return response.data.issues.map(transformDefect);
-  }
-  catch (err) {
+  } catch (err) {
     console.log('Error in getJiraDefects: ' + err);
+    return [];
   }
 }
 
 function createJiraSearchRequestBody () {
   return {
-    'jql':JIRA_JQL,
-    'startAt':0,
-    'maxResults':JIRA_MAX_RESULTS,
+    'jql': JIRA_JQL,
+    'startAt': 0,
+    'maxResults': JIRA_MAX_RESULTS,
     'fields': [
       'id',
       'key',
@@ -60,15 +69,31 @@ function createJiraSearchRequestBody () {
       'reporter',
       'description'
     ]
-  }
+  };
 }
 
-function transformDefect(defect) {
-    let key = defect.key;
-    let url = JIRA_BASE_URI + `/browse/${defect.key}`;
-    let link = createHTMLLink(url,url);
-    let title = `P${defect.fields.priority.id}   ${defect.key}  ${escape(defect.fields.summary)}`;
-    let description = [
+function transformDefect (defect) {
+  let key = defect.key;
+  let url = JIRA_BASE_URI + `/browse/${defect.key}`;
+  let link = createHTMLLink(url, url);
+  let priority = (defect.fields.priority && defect.fields.priority.id) ? parseInt(defect.fields.priority.id, 10) : '?';
+
+  if (typeof priority !== 'number' || priority >= JIRA_UNSET_PRIORITY_VALUE) {
+    priority = '?';
+  }
+
+  let title = `P${priority} - ${defect.key} - ${escape(defect.fields.summary)}`;
+  let description;
+
+  if (V1_TASK_DETAIL === 'short') {
+    description = [
+      `URL: ${link}`,
+      `Description:`,
+      `<![CDATA[${escape(defect.fields.description)}]]>`,
+      `Assignee: ${defect.fields.assignee.displayName}`
+    ].join(`<![CDATA[<br/>]]>`);
+  } else {
+    description = [
       `Key: ${defect.key}`,
       `Summary: ${escape(defect.fields.summary)}`,
       `Priority: ${defect.fields.priority.name}`,
@@ -79,18 +104,18 @@ function transformDefect(defect) {
       `Description:`,
       `<![CDATA[${escape(defect.fields.description)}]]>`
     ].join(`<![CDATA[<br/>]]>`);
-  return {key,url,title,description};
+  }
+  return {key, url, title, description};
 }
 
-function createHTMLLink(href,text) {
+function createHTMLLink (href, text) {
   return `<![CDATA[<a href=${href}>${text}</a>]]>`;
 }
 
-async function createV1TasksFromDefects(defects) {
-  try { 
+async function createV1TasksFromDefects (defects) {
+  try {
     defects = await filterOutPreviouslyProcessedDefects(defects);
-    if (defects.length == 0) 
-      return {};
+    if (defects.length === 0) { return {}; }
 
     const allPromises = defects.map(createNewV1Task);
     let tasks = await Promise.all(allPromises);
@@ -100,81 +125,83 @@ async function createV1TasksFromDefects(defects) {
   }
 }
 
-async function filterOutPreviouslyProcessedDefects(defects) {
+async function filterOutPreviouslyProcessedDefects (defects) {
   try {
-    let searchRequestBody=createV1FindTasksInStoryRequestBody();
-    const response = await axios.post(V1_QUERY_STORY_URI,searchRequestBody);
+    let searchRequestBody = createV1FindTasksInStoryRequestBody();
+    const response = await axios.post(V1_QUERY_STORY_URI, searchRequestBody);
     let tasks = response.data[0];
 
-    defects = applyFilter(defects,tasks);
+    defects = applyFilter(defects, tasks);
 
     return defects;
   } catch (err) {
     console.log('Error in filterOutPreviouslyProcessedDefects: ' + err);
+    return [];
   }
 }
 
-function createV1FindTasksInStoryRequestBody() {
+function createV1FindTasksInStoryRequestBody () {
   return {
-      "from": "Task",
-      "select": [
-          "Number",
-          "Name",
-      ],
-      "where": {
-          "Parent.ID":"Story:"+STORY_ID
-      },
-      "page": {
-          "start": 0,
-          "size": V1_MAX_RESULTS    
-      }
+    'from': 'Task',
+    'select': [
+      'Number',
+      'Name'
+    ],
+    'where': {
+      'Parent.ID': 'Story:' + V1_STORY_ID
+    },
+    'page': {
+      'start': 0,
+      'size': V1_MAX_RESULTS
+    }
   };
 }
 
-function applyFilter(defects,tasks) {  
-  return defects.filter(defect =>  !tasks.some(task => task.Name.includes(defect.key))) 
+function applyFilter (defects, tasks) {
+  return defects.filter(defect => !tasks.some(task => task.Name.includes(defect.key)));
 }
 
-async function createNewV1Task(defect) {
+async function createNewV1Task (defect) {
   try {
     let taskRequestBody = createNewV1TaskRequestBody(defect);
-    const taskResponse = await axios.post(V1_REST_TASK_URI,taskRequestBody);
+    const taskResponse = await axios.post(V1_REST_TASK_URI, taskRequestBody);
     let taskId = taskResponse.data.id;
-    taskId=taskId.split(':')[1];
-   
-    const linkId = await createNewV1Link(defect,taskId);
-    
-    return `${taskId}`;
+    taskId = taskId.split(':')[1];
+
+    const linkId = await createNewV1Link(defect, taskId);
+
+    return `${taskId}: ${defect.title}`;
   } catch (err) {
     console.log('Error in createNewV1Task ' + err);
   }
 }
 
-function createNewV1TaskRequestBody(defect) {
+function createNewV1TaskRequestBody (defect) {
   return [
-      `<Asset href="/rest-1.v1/New/Task">`,
-      `<Attribute name="Name" act="set">${defect.title}</Attribute>`,
-      `<Attribute name="Description" act="set">${defect.description}</Attribute>`,
-      `<Relation name="Parent" act="set">`,
-      `<Asset href="/rest-1.v1/Data/Story/${STORY_ID}" idref="Story:${STORY_ID}" />`,
-      `</Relation>`,
-      `</Asset>`
+    `<Asset href="/rest-1.v1/New/Task">`,
+    `<Attribute name="Name" act="set">${defect.title}</Attribute>`,
+    `<Attribute name="Description" act="set">${defect.description}</Attribute>`,
+    // set task hours with V1_TASK_HOURS
+    `<Relation name="Parent" act="set">`,
+    `<Asset href="/rest-1.v1/Data/Story/${V1_STORY_ID}" idref="Story:${V1_STORY_ID}" />`,
+    `</Relation>`,
+    `</Asset>`
   ].join('');
-  }
+}
 
-async function createNewV1Link(defect,taskId) {
+async function createNewV1Link (defect, taskId) {
   try {
-    let linkRequestBody = createNewV1LinkRequestBody('JIRA',defect.url,taskId)
+    let linkRequestBody = createNewV1LinkRequestBody('JIRA', defect.url, taskId);
     let linkResponse = await axios.post(V1_REST_LINK_URI, linkRequestBody);
     let linkId = linkResponse.data.id;
-    linkId=linkId.split(':')[1];
+    linkId = linkId.split(':')[1];
     return linkId;
   } catch (err) {
     console.log('Error in createNewV1Link ' + err);
   }
 }
 
-function createNewV1LinkRequestBody(name,url,taskId) {
+function createNewV1LinkRequestBody (name, url, taskId) {
   return [
     `<Asset href="/rest-1.v1/New/Link">`,
     `<Attribute name="OnMenu" act="set">true</Attribute>`,
@@ -186,3 +213,9 @@ function createNewV1LinkRequestBody(name,url,taskId) {
     `</Asset>`
   ].join('');
 }
+
+/* TODO:
+Use V1_TASK_HOURS to set the hour estimate and remaining hours on the created tasks
+Figure out how to get tasks created in the sort order returned by the JIRA query
+Enhance error handling
+*/
